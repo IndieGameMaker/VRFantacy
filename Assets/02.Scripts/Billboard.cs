764f8d115a81f204d9f85248fff44b16137e31c7
Jaehyun Lee <myleje@gmail.com>
2019-12-14 15:24:33 +0900

    .

---
+++ Assets/02.Scripts/Billboard.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    private Transform camTr;
+    private Transform tr;
+    
+    void Start()
+    {
+        tr = GetComponent<Transform>(); //캔버스의 Transform
+        camTr = Camera.main.transform;  //카메라의 Transform
+    }
+
+    void LateUpdate()
+    {
+        tr.LookAt(camTr.position);
+    }
+}
