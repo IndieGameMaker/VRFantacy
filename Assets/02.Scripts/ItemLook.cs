970ca5c4dc3f0aa810d07b41ce5304796a37d4e0
Jaehyun Lee <myleje@gmail.com>
2019-12-14 14:47:29 +0900

    .

--- Assets/02.Scripts/ItemLook.cs
+++ Assets/02.Scripts/ItemLook.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class ItemLook : MonoBehaviour
 {
     public void OnLookItem(bool isLooked)
     {
         //Debug.Log(isLooked);
-        MoveCtrl.isStopped = isLooked;
+        //MoveCtrl.isStopped = isLooked;
     }
 }
