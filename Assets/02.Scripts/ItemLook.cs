ed1a38d2f1fbb24cd335bbd962394047138852a8
Jaehyun Lee <myleje@gmail.com>
2019-12-14 14:55:37 +0900

    .

--- Assets/02.Scripts/ItemLook.cs
+++ Assets/02.Scripts/ItemLook.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class ItemLook : MonoBehaviour
+public class ItemLook : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public void OnLookItem(bool isLooked)
     {
         //Debug.Log(isLooked);
         //MoveCtrl.isStopped = isLooked;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        MoveCtrl.isStopped = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+       MoveCtrl.isStopped = false;
     }
 }
