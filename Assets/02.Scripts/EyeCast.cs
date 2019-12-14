f1f4072a5c0986646dee67d0c4dc4fa4230aefc8
Jaehyun Lee <myleje@gmail.com>
2019-12-14 16:19:06 +0900

    .

--- Assets/02.Scripts/EyeCast.cs
+++ Assets/02.Scripts/EyeCast.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class EyeCast : MonoBehaviour
 {
+    private Ray ray;
+    private RaycastHit hit;
+
+    [Range(5.0f, 20.0f)]
+    public float range = 10.0f;
+
     // Start is called before the first frame update
     void Start()
     {
