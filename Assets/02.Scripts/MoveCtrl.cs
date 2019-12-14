3dd4aa57c8e6b01ddbfcb3525a93ef15a1edfad9
Jaehyun Lee <myleje@gmail.com>
2019-12-14 11:29:55 +0900

    .

--- Assets/02.Scripts/MoveCtrl.cs
+++ Assets/02.Scripts/MoveCtrl.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class MoveCtrl : MonoBehaviour
 {
+    public enum MoveType
+    {
+        WAY_POINT, LOOK_AT
+    }
+    
+    //이동방식을 결정하는 변수
+    public MoveType moveType = MoveType.WAY_POINT;
+    //웨이 포인트의 Transform 저장할 배열
+    public Transform[] points;
+
     // Start is called before the first frame update
     void Start()
     {
